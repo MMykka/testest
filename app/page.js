510326aa3e23fffad1ed3a6527047471661fbcd7
Mykkaa <michaelaboukhalil@gmail.com>
2023-09-28 13:52:16 +0300

    first commit

--- app/page.js
+++ app/page.js
@@ -59,6 +59,21 @@ export default function Home() {
       <div className='bg-gray-500 w-full'>
         <div className='flex flex-col w-full mx-auto'>
           <div className='flex justify-center gap-[60px] mt-[160px]'>
+          <img src='nissan1.jpeg' className='w-1/3'/>
+          <img src='nissan2.jpeg' className='w-1/3'/>
+          </div>
+          <div>
+            <br></br>
+            <h1 className='flex justify-center font-bold text-white'>Nissan R34GT-R</h1>
+            <h1 className='flex justify-center font-bold text-white'>T shirt</h1>
+            <h1 className='flex justify-center font-bold text-white'>$25 USD</h1>
+            <h1 className='flex justify-center font-bold text-white'>Colors: available in black and White</h1>
+          </div>
+        </div>
+      </div>
+      <div className='bg-gray-500 w-full'>
+        <div className='flex flex-col w-full mx-auto'>
+          <div className='flex justify-center gap-[60px] mt-[160px]'>
           <img src='por4.jpg' className='w-1/3'/>
           <img src='por3.jpg' className='w-1/3'/>
           </div>
@@ -77,6 +92,7 @@ export default function Home() {
         </div>
       </div>
       </div>
+      
     </div>
   )
 }
